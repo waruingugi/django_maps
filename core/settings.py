c151a45a5a18ada2d5bf8edaf384cbf76c70719a
waruingugi <waruingugientp@gmail.com>
2021-09-05 14:06:25 +0300

    Added tilt

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eux^!3on*526*)0%$^(=v$t^jz746rcj(twvp1y%^9tsk%d5#o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
